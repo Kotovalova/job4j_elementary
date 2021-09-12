a7f644a5c07a330f33ba983d6f63e0d5aa2185a7
maria <kotovalovam@yandex.ru>
2021-09-12 19:03:06 +0700

    Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class StudentInfo {
     public static void main(String[] args) {
-        System.out.println("Мария Котовалова");
+        System.out.println("Котовалова Мария");
         System.out.println("16.05.1994");
     }
 }
