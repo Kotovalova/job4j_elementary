107115e9174a76e10e1bb0614d92742cf6519c84
maria <kotovalovam@yandex.ru>
2021-09-27 17:07:52 +0700

    Поиск индекса в диапазоне. [#156318 #208653]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -17,7 +17,7 @@ public class FindLoop {
         for (int i = start; i <= finish; i++) {
             if (data[i] == el) {
                 rst = i;
-                break; /*комент*/
+                break;
             }
         }
         return rst;
